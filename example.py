ceb97c8e93afb332d8052fe6c3041aad9a2eb686
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-08-22 21:13:31 +0800

    нэг мөрөөс олон хувьсагчид утга авах

--- example.py
+++ example.py
@@ -1,2 +1,6 @@
-a, b, c = [int(x) for x in raw_input().split()]
-print(a + b + c)
+# Python 3
+var1, var2 = [int(x) for x in input().split()]
+print(var1 + var2)
+# Python 2
+var1, var2 = [int(x) for x in raw_input().split()]
+print(var1 + var2)
