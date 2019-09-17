2be2e114c1e2f1cd24d0a658865c274c8d9d3afe
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-09-17 19:36:12 +0800

    func update

--- Бие-даалт/func.py
+++ Бие-даалт/func.py
@@ -1,39 +1,39 @@
 import json
-import json
+
 def menuGarga():
     print('Цэс')
-    print('----------------------------')
-    print("1. Мэдээлэл харах")
-    print("2. Мэдээлэл хайх")
-    print("3. Мэдээлэл бүртгэх")
-    print("4. Мэдээлэл устгах")
-    print("5. Мэдээлэл засварлах")
-    print("6. Гарах")
-    print('----------------------------')
+    print('------------------------------')
+    print("|    1. Мэдээлэл харах       |")
+    print("|    2. Мэдээлэл хайх        |")
+    print("|    3. Мэдээлэл бүртгэх     |")
+    print("|    4. Мэдээлэл устгах      |")
+    print("|    5. Мэдээлэл засварлах   |")
+    print("|    6. Гарах                |")
+    print('------------------------------')
     
-    f = open("data.txt","rt")
-    data = f.read() # json => dict
-    data = json.loads(data) # load
-    
-    too = inputNumber()
+    too = int(input())
     if too == 1: 
-        infoData(data)
+        infoData()
     elif too == 2:
-        searchData(data)
+        searchData()
     elif too == 3:
-        registerData(data)
+        registerData()
     elif too == 6:
-        f.close()
         print("Programaas garlaa!")
 
-def inputNumber():
-    return int(input())
 
-def infoData(data): # list
+def infoData():
+    f = open("data.txt","rt")
+    data = f.read()
+    data = json.loads(data) # str => list
     for i in data:
         print(i["owog"],i["ner"],i["nas"],i["huis"],i["utas"])
+    f.close()
 
-def registerData(data): # list
+def registerData():
+    f = open("data.txt","rt")
+    data = f.read()
+    data = json.loads(data) # str => list
     owog = input("Owog:")
     ner = input("Ner:")
     nas = input("Nas:")
@@ -48,13 +48,16 @@ def registerData(data): # list
         "utas": utas
     }
     data.append(person)
-    data = json.dumps(data) # dict => str
+    data = json.dumps(data) # list => str
     f = open("data.txt","w")
     f.write(data)
     f.close()
     print("Амжилттай бүртгэгдлээ")
 
-def searchData(data):
+def searchData():
+    f = open("data.txt","rt")
+    data = f.read()
+    data = json.loads(data) # str => list
     print('Хайх цэс')
     print('----------------------------')
     print("1. Нэрээр хайх")
@@ -63,6 +66,7 @@ def searchData(data):
     print("4. Гарах")
     print('----------------------------')
     n = int(input())
+    
     if n==1:
         ner = input("Хайх нэр оруулах: ")
         for i in data:
@@ -70,7 +74,7 @@ def searchData(data):
                 print(i["owog"],i["ner"],i["nas"],i["huis"],i["utas"])
             else:
                 print("Мэдээлэл олдсонгүй! ")
-                break
+            break
     elif n==2:
         utas = input("Утасны дугаар оруулна уу:")
         for i in data:
@@ -78,6 +82,10 @@ def searchData(data):
                 print(i["owog"],i["ner"],i["nas"],i["huis"],i["utas"])
             else:
                 print("Мэдээлэл олдсонгүй! ")
-                break
+            break
     elif n==3:
+        f.close()
         menuGarga()
+    else:
+        print('Программаас гарлаа')
+    f.close()
