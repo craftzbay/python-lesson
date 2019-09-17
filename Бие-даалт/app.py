d6923380aa672cd7ca005bd305341af269748945
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-09-17 20:58:17 +0800

    app

--- Бие-даалт/app.py
+++ Бие-даалт/app.py
@@ -1,2 +1,8 @@
 import func
-func.menuGarga()
+
+username = input("Хэрэглэгчийн нэр: ")
+password = input("Нууц үг: ")
+if username == 'admin' and password == 'password':
+    func.menuGarga()
+else:
+    print('Хэрэглэгчийн нэр, нууц үг буруу байна! ')
