a21d127746497e17c9d9dd2a7d359a70445348bc
Bayarsaikhan <bayraa.3033@gmail.com>
2019-09-24 15:56:50 +0800

    өгөгдлийн төрөл

---
+++ 00 start/02_data_type.py
@@ -0,0 +1,19 @@
+x = 1    # int
+y = 2.8  # float
+
+# x болон y тооны төрлийг хэвлэх
+print(type(x))
+print(type(y))
+
+# int төрлийг float төрөлт хөрвүүлэх
+a = float(x)
+
+# float төрлийг int төрөлт хөрвүүлэх
+b = int(y)
+
+print(a)
+print(b)
+
+# a болон b тооны төрлийг хэвлэх
+print(type(a))
+print(type(b))
