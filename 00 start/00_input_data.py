9d677248256b8ad74b37dc0fc017bac5448c5dea
Bayarsaikhan <bayraa.3033@gmail.com>
2019-09-24 17:16:47 +0800

    Нэг мөрөөс олон утга авах

--- 00 start/00_input_data.py
+++ 00 start/00_input_data.py
@@ -25,5 +25,5 @@ print("Нэр: " + ner + " , " + "Нас: " + nas)
 var1, var2 = [int(x) for x in input().split()]
 print(var1 + var2)
 # Python 2
-var1, var2 = [int(x) for x in raw_input().split()]
+# var1, var2 = [int(x) for x in raw_input().split()]
 print(var1 + var2)
