8a566dc5a269bf17424ccab9c2965c869ac3b2dc
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-09-24 21:11:35 +0800

    data

--- 07 tkinter/programm/app.py
+++ 07 tkinter/programm/app.py
@@ -1,4 +1,18 @@
 from tkinter import Tk, Button, Menu, Label
+from tkinter import ttk
+
+def show():
+
+    tempList = [
+        ['Jim', '0.10'], 
+        ['Dave', '0.20'], 
+        ['James', '0.20'], 
+        ['Eden', '0.5']]
+        
+    tempList.sort(key=lambda e: e[1], reverse=True)
+
+    for i, (name, score) in enumerate(tempList, start=1):
+        listBox.insert("", "end", values=(i, name, score))
 
 
 def menuClicked():
@@ -6,7 +20,8 @@ def menuClicked():
 
 window = Tk()
 window.title('Бүртгэлийн программ')
-window.geometry('400x500')
+window.geometry('805x500')
+window.resizable(0,0)
 
 
 menubar = Menu(window)
@@ -18,7 +33,18 @@ menu.add_separator()
 menu.add_command(label="Гарах", command=window.quit)
 menubar.add_cascade(label="Цэс", menu=menu)
 
-Label(window, text="Мэдээллийн жагсаалт", 
-    font=("Arial",14)).grid(row=0, columnspan=3)
+Label(window, text="Мэдээллийн жагсаалт", font=("Arial",14)).grid(row=0, columnspan=3)
 window.config(menu=menubar)
+
+# create Treeview with 3 columns
+cols = ('Овог', 'Нэр','Нас', 'Утас')
+listBox = ttk.Treeview(window, columns=cols, show='headings')
+# set column headings
+for col in cols:
+    listBox.heading(col, text=col)    
+listBox.grid(row=1, column=0, columnspan=2)
+
+Button(window, text="Show scores", width=15, command=show).grid(row=4, column=0)
+Button(window, text="Close", width=15, command=exit).grid(row=4, column=1)
+
 window.mainloop()
