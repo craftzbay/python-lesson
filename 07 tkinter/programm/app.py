b821b42c644ea22b60c2a1f5ab6a1e181fba3f39
Bayarsaikhan <bayraa.3033@gmail.com>
2019-09-26 18:14:24 +0800

    APP

--- 07 tkinter/programm/app.py
+++ 07 tkinter/programm/app.py
@@ -1,13 +1,14 @@
 from tkinter import Tk, Button, Menu, Label
 from tkinter import ttk
 
+
 def showData():
 
     data = [
-        ['Bat', 'Bold','26','99455623'], 
-        ['Bat', 'Bold','26','99455623'], 
-        ['Bat', 'Bold','26','99455623'],  
-        ['Bat', 'Bold','26','99455623']
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623']
     ]
 
     # data.sort(key=lambda e: e[0], reverse=True)
@@ -19,10 +20,11 @@ def showData():
 def menuClicked():
     print('menu daragdsan')
 
+
 window = Tk()
 window.title('Бүртгэлийн программ')
 window.geometry('805x500')
-window.resizable(0,0)
+window.resizable(0, 0)
 
 
 menubar = Menu(window)
@@ -34,14 +36,15 @@ menu.add_separator()
 menu.add_command(label="Гарах", command=window.quit)
 menubar.add_cascade(label="Цэс", menu=menu)
 
-Label(window, text="Мэдээллийн жагсаалт", font=("Arial",14)).grid(row=0, columnspan=3)
+Label(window, text="Мэдээллийн жагсаалт", font=(
+    "Arial", 14)).grid(row=0, columnspan=3)
 window.config(menu=menubar)
 
 
-cols = ('Овог', 'Нэр','Нас', 'Утас')
+cols = ('Овог', 'Нэр', 'Нас', 'Утас')
 listBox = ttk.Treeview(window, columns=cols, show='headings')
 for col in cols:
-    listBox.heading(col, text=col)    
+    listBox.heading(col, text=col)
 listBox.grid(row=2, column=0, columnspan=2)
 
 showData()
