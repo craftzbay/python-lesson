bc16518da5417c9e947905efeaa6b092b4a9bef2
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-09-26 20:41:29 +0800

    dialog nemsen

--- 07 tkinter/15_dialog.py
+++ 07 tkinter/15_dialog.py
@@ -1,27 +1,113 @@
-import tkinter as tk
+from tkinter import *
+import tkinter.ttk as ttk
 from tkinter import simpledialog
+import tkinter.messagebox as tkMessageBox
 
-application_window = tk.Tk()
+data = [
+    {
+        "owog":"Bat",
+        "ner":"Bold",
+        "nas": 26,
+        "huis": "er",
+        "utas": "99562312"
+    },
+    {
+        "owog":"Bat",
+        "ner":"Bold",
+        "nas": 26,
+        "huis": "er",
+        "utas": "99562312"
+    },
+    {
+        "owog":"Bat",
+        "ner":"Bold",
+        "nas": 26,
+        "huis": "er",
+        "utas": "99562312"
+    }
+]
 
-answer = simpledialog.askstring("Input", "What is your first name?",
-                                parent=application_window)
-if answer is not None:
-    print("Your first name is ", answer)
-else:
-    print("You don't have a first name?")
 
-answer = simpledialog.askinteger("Input", "What is your age?",
-                                 parent=application_window,
-                                 minvalue=0, maxvalue=100)
-if answer is not None:
-    print("Your age is ", answer)
-else:
-    print("You don't have an age?")
+root = Tk()
+root.title("Python: Simple CRUD Applition")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+width = 565
+height = 400
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+root.resizable(0, 0)
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
-answer = simpledialog.askfloat("Input", "What is your salary?",
-                               parent=application_window,
-                               minvalue=0.0, maxvalue=100000.0)
-if answer is not None:
-    print("Your salary is ", answer)
-else:
-    print("You don't have a salary?")
+
+# Frames
+top_frame = Frame(root, bg='white', width=450, height=30, pady=3).grid(row=0, sticky="ew")
+center = Frame(root, bg='gray', width=50, height=40, padx=3, pady=3).grid(row=1, sticky="nsew")
+
+
+def createData():
+    owog = simpledialog.askstring(
+    "Input", "Овог нэрээ оруулна уу",
+                                    parent=root)
+    ner = simpledialog.askstring(
+    "Input", "Нэрээ оруулна уу",
+                                    parent=root)
+    nas = simpledialog.askstring(
+    "Input", "Нас оруулна уу",
+                                    parent=root)
+    huis = simpledialog.askstring(
+    "Input", "Хүйс оруулна уу",
+                                    parent=root)
+    utas = simpledialog.askstring(
+    "Input", "Утас оруулна уу",
+                                    parent=root)                               
+
+
+def menuClicked():
+    print('menu daragdsan')
+
+menubar = Menu(root)
+menu = Menu(menubar, tearoff=0)
+menu.add_command(label="Бүртгэх", command=createData)
+menu.add_command(label="Засварлах", command=menuClicked)
+menu.add_command(label="Устгах", command=menuClicked)
+menu.add_separator()
+menu.add_command(label="Гарах", command=root.quit)
+menubar.add_cascade(label="Цэс", menu=menu)
+# display the menu
+root.config(menu=menubar)
+
+
+model_label = Label(top_frame, text='Жагсаалт', font=(
+    "Arial", 14))
+model_label.grid(row=0, columnspan=3)
+
+
+table = ttk.Treeview(
+    center, columns=("Овог", "Нэр", "Нас", "Хүйс", "Утас"),
+                    selectmode="extended", height=500)
+
+table.heading('Овог', text="Овог", anchor=W)
+table.heading('Нэр', text="Нэр", anchor=W)
+table.heading('Нас', text="Нас", anchor=W)
+table.heading('Хүйс', text="Хүйс", anchor=W)
+table.heading('Утас', text="Утас", anchor=W)
+table.column('#0', stretch=NO, minwidth=0, width=0)
+table.column('#1', stretch=NO, minwidth=0, width=80)
+table.column('#2', stretch=NO, minwidth=0, width=120)
+table.column('#3', stretch=NO, minwidth=0, width=80)
+table.column('#4', stretch=NO, minwidth=0, width=150)
+table.column('#5', stretch=NO, minwidth=0, width=120)
+table.grid(row=1,column=0)
+
+def Read():
+    table.delete(*table.get_children())
+
+    for d in data:
+        table.insert('', 'end', values=(d["owog"], d["ner"], d["nas"], d["huis"], d["utas"]))
+
+
+Read()
+root.mainloop()
