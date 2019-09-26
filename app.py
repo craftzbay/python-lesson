dab9706448417154c5582391d15e08a40a6dd678
Bayarsaikhan <bayraa.3033@gmail.com>
2019-09-26 18:14:28 +0800

    nice

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
-import tkinter as tk
-from tkinter import simpledialog
+from tkinter import Tk, Button, Menu, Label
+from tkinter import ttk
 
-application_window = tk.Tk()
 
-answer = simpledialog.askstring("Input", "What is your first name?",
-                                parent=application_window)
-if answer is not None:
-    print("Your first name is ", answer)
-else:
-    print("You don't have a first name?")
+def showData():
 
-answer = simpledialog.askinteger("Input", "What is your age?",
-                                 parent=application_window,
-                                 minvalue=0, maxvalue=100)
-if answer is not None:
-    print("Your age is ", answer)
-else:
-    print("You don't have an age?")
+    data = [
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623'],
+        ['Bat', 'Bold', '26', '99455623']
+    ]
 
-answer = simpledialog.askfloat("Input", "What is your salary?",
-                               parent=application_window,
-                               minvalue=0.0, maxvalue=100000.0)
-if answer is not None:
-    print("Your salary is ", answer)
-else:
-    print("You don't have a salary?")
+    # data.sort(key=lambda e: e[0], reverse=True)
+
+    for i in data:
+        listBox.insert("", "end", values=i)
+
+
+def menuClicked():
+    print('menu daragdsan')
+
+
+window = Tk()
+window.title('Бүртгэлийн программ')
+window.geometry('805x500')
+window.resizable(0, 0)
+
+
+menubar = Menu(window)
+menu = Menu(menubar, tearoff=0)
+menu.add_command(label="Бүртгэх", command=menuClicked)
+menu.add_command(label="Засварлах", command=menuClicked)
+menu.add_command(label="Устгах", command=menuClicked)
+menu.add_separator()
+menu.add_command(label="Гарах", command=window.quit)
+menubar.add_cascade(label="Цэс", menu=menu)
+
+Label(window, text="Мэдээллийн жагсаалт", font=(
+    "Arial", 14)).grid(row=0, columnspan=3)
+window.config(menu=menubar)
+
+
+cols = ('Овог', 'Нэр', 'Нас', 'Утас')
+listBox = ttk.Treeview(window, columns=cols, show='headings')
+for col in cols:
+    listBox.heading(col, text=col)
+listBox.grid(row=2, column=0, columnspan=2)
+
+showData()
+window.mainloop()
