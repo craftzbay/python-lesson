4f2e9e0996f86b3d0d09257e4654891e0fead992
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2019-09-26 19:19:38 +0800

    frame grid example

--- 07 tkinter/04_frame.py
+++ 07 tkinter/04_frame.py
@@ -1,22 +1,48 @@
 from tkinter import *
 
 root = Tk()
-frame = Frame(root)
-frame.pack()
+root.title('Model Definition')
+root.geometry('{}x{}'.format(460, 350))
 
-bottomframe = Frame(root)
-bottomframe.pack(side=BOTTOM)
+# create all of the main containers
+top_frame = Frame(root, bg='cyan', width=450, height=50, pady=3)
+center = Frame(root, bg='gray2', width=50, height=40, padx=3, pady=3)
+btm_frame = Frame(root, bg='white', width=450, height=45, pady=3)
+btm_frame2 = Frame(root, bg='lavender', width=450, height=60, pady=3)
 
-redbutton = Button(frame, text="Red", fg="red")
-redbutton.pack(side=LEFT)
+# layout all of the main containers
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
-greenbutton = Button(frame, text="Brown", fg="brown")
-greenbutton.pack(side=LEFT)
+top_frame.grid(row=0, sticky="ew")
+center.grid(row=1, sticky="nsew")
+btm_frame.grid(row=3, sticky="ew")
+btm_frame2.grid(row=4, sticky="ew")
 
-bluebutton = Button(frame, text="Blue", fg="blue")
-bluebutton.pack(side=LEFT)
+# create the widgets for the top frame
+model_label = Label(top_frame, text='Model Dimensions')
+width_label = Label(top_frame, text='Width:')
+length_label = Label(top_frame, text='Length:')
+entry_W = Entry(top_frame, background="pink")
+entry_L = Entry(top_frame, background="orange")
 
-blackbutton = Button(bottomframe, text="Black", fg="black")
-blackbutton.pack(side=BOTTOM)
+# layout the widgets in the top frame
+model_label.grid(row=0, columnspan=3)
+width_label.grid(row=1, column=0)
+length_label.grid(row=1, column=2)
+entry_W.grid(row=1, column=1)
+entry_L.grid(row=1, column=3)
+
+# create the center widgets
+center.grid_rowconfigure(0, weight=1)
+center.grid_columnconfigure(1, weight=1)
+
+ctr_left = Frame(center, bg='blue', width=100, height=190)
+ctr_mid = Frame(center, bg='yellow', width=250, height=190, padx=3, pady=3)
+ctr_right = Frame(center, bg='green', width=100, height=190, padx=3, pady=3)
+
+ctr_left.grid(row=0, column=0, sticky="ns")
+ctr_mid.grid(row=0, column=1, sticky="nsew")
+ctr_right.grid(row=0, column=2, sticky="ns")
 
 root.mainloop()
