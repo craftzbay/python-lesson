55fffbd5b0af8c0fffbe439666a5dc965f7b3bb3
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2020-05-30 11:04:25 +0800

    Үндсэн программ

--- Бие даалт/app.py
+++ Бие даалт/app.py
@@ -1,8 +1,16 @@
-import func
+from function import showMenu, create, remove
 
-username = input("Хэрэглэгчийн нэр: ")
-password = input("Нууц үг: ")
-if username == 'admin' and password == 'password':
-    func.menuGarga()
+showMenu()
+
+n = int(input('Үйдлийн дугаар оруулна уу: '))
+
+if n == 1:
+    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
+elif n == 2:
+    create()
+elif n == 3:
+    remove()
+elif n == 4:
+    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
 else:
-    print('Хэрэглэгчийн нэр, нууц үг буруу байна! ')
+    print('Та программаас гарлаа...')
