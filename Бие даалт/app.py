670ba50108f979b3000137f785d10d541b363eb6
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2020-05-31 10:42:35 +0800

    үндсэн программ

--- Бие даалт/app.py
+++ Бие даалт/app.py
@@ -1,16 +1,18 @@
-from function import showMenu, create, remove
+from function import showMenu, create, remove, show, update
 
-showMenu()
+n = 1
 
-n = int(input('Үйдлийн дугаар оруулна уу: '))
-
-if n == 1:
-    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
-elif n == 2:
-    create()
-elif n == 3:
-    remove()
-elif n == 4:
-    print('Та өөр дугаар оруулна уу энэ үйлдэл гэрийн даалгавар')
-else:
-    print('Та программаас гарлаа...')
+while n > 0:
+    showMenu()
+    n = int(input('Үйдлийн дугаар оруулна уу: '))
+    if n == 1:
+        show()
+    elif n == 2:
+        create()
+    elif n == 3:
+        remove()
+    elif n == 4:
+        update()
+    else:
+        print('Та программаас гарлаа...')
+        break
