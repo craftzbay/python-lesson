dff782ad0d371dbf41deab776abef06a64174024
Bayarsaikhan Otgonbayar <bayraa.3033@gmail.com>
2020-05-31 10:43:00 +0800

    функцууд

--- Бие даалт/function.py
+++ Бие даалт/function.py
@@ -1,5 +1,11 @@
+def show():
+    f = open('dun.txt', 'r')
+    txt = f.read()
+    f.close()
+    print(txt)
+
 def showMenu():
-    print('Дүнгийн бүртгэлийн программ\n','-'*30)
+    print('\n\n\n','-'*30,'\nДүнгийн бүртгэлийн программ\n','-'*30)
     print('1. Дүнгийн мэдээлэл харах')
     print('2. Дүнгийн мэдээлэл бүртгэх')
     print('3. Дүнгийн мэдээлэл устгах')
@@ -25,3 +31,23 @@ def remove():
     f = open('dun.txt','w')
     f.write(txt)
     f.close()
+
+
+def update():
+    f = open('dun.txt', 'r')
+    txt = f.read()
+    f.close()
+    data = txt.split('\n')
+    owog, ner = input('Овог, нэр: ').split()
+    for item in data: # 'uuu aaa 88'
+        row = item.split() # ['uuu', 'aaa', '88']
+        if row[0] == owog and row[1] == ner:
+            dun = input('засах дүн оруулна уу: ')
+            row[2] = dun
+            index = data.index(item)
+            data[index] = ' '.join(row)
+
+    f = open('dun.txt','w')
+    f.write('\n'.join(data))
+    f.close()
+    print('\nДүнгийн мэдээлэл амжилттай заслаа.\n')
